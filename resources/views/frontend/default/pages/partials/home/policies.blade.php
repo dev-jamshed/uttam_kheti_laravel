5d6922a3b79b57fc439e1f42baa1bc896daf2b94
dev-jamshed <jamshedgopang283@gmail.com>
2025-02-21 14:50:12 +0500

    first commit

---
+++ resources/views/frontend/default/pages/partials/home/policies.blade.php
@@ -0,0 +1,59 @@
+<section class="policy-container">
+    <div class="container">
+        <div class="row gy-4">
+    
+            {{-- Policy Card --}}
+            <div class="col-lg-3 col-md-4 col-6">
+                <div class="policy-card">
+                    <div class="icon-container">
+                        <i class="fa-light fa-truck-bolt"></i>
+                    </div>
+                    <div class="policy-card-content">
+                        <h3>Same Day Free Delivery</h3>
+                        <p>Fast & free shipping on all orders.</p>
+                    </div>
+                </div>
+            </div>
+    
+            {{-- Policy Card --}}
+            <div class="col-lg-3 col-md-4 col-6">
+                <div class="policy-card">
+                    <div class="icon-container">
+                        <i class="fa-light fa-credit-card"></i>
+                    </div>
+                    <div class="policy-card-content">
+                        <h3>Secure Payment</h3>
+                        <p>Safe & encrypted transactions.</p>
+                    </div>
+                </div>
+            </div>
+    
+            {{-- Policy Card --}}
+            <div class="col-lg-3 col-md-4 col-6">
+                <div class="policy-card">
+                    <div class="icon-container">
+                        <i class="fa-light fa-right-left"></i>
+                    </div>
+                    <div class="policy-card-content">
+                        <h3>Same Day Return</h3>
+                        <p>Quick & easy return policy.</p>
+                    </div>
+                </div>
+            </div>
+    
+            {{-- Policy Card --}}
+            <div class="col-lg-3 col-md-4 col-6">
+                <div class="policy-card">
+                    <div class="icon-container">
+                        <i class="fa-light fa-headset"></i>
+                    </div>
+                    <div class="policy-card-content">
+                        <h3>24/7 Support</h3>
+                        <p>Help is available anytime.</p>
+                    </div>
+                </div>
+            </div>
+    
+        </div>
+    </div>
+</section>
